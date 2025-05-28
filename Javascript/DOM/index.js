7c45c437a256092aa2458e626ded1de8957486dd
JULFYKO <julfyko@gmail.com>
2025-05-28 20:45:22 +0300

    add

---
+++ Javascript/DOM/index.js
@@ -0,0 +1,14 @@
+document.getElementById('generate-btn').onclick = function () {
+    const randomNumber = Math.floor(Math.random() * 101);
+    document.getElementById('result').textContent = randomNumber;
+};
+
+const toggleBtn = document.getElementById('toggle-btn');
+const toggleText = document.getElementById('toggle-text');
+toggleBtn.onclick = function () {
+    if (toggleText.style.display === 'none') {
+        toggleText.style.display = '';
+    } else {
+        toggleText.style.display = 'none';
+    }
+};
